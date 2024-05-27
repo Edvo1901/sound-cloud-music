ea900edcaf521f6fd11e9cbd4bc3b0ebdd0d1d5e
Edvo1901 <DongVo1901@gmail.com>
2024-05-27 10:52:43 +0930

    customise login session and token data

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github";
 import { AuthOptions } from "next-auth";
+import { JWT } from "next-auth/jwt";
+import { sendRequest } from "@/utils/API";
 
 export const authOptions: AuthOptions = {
 	secret: process.env.NO_SECRET,
@@ -11,6 +13,35 @@ export const authOptions: AuthOptions = {
 			clientSecret: process.env.GITHUB_SECRET!,
 		}),
 	],
+	callbacks: {
+		async jwt({ token, user, account, profile, trigger }) {
+			if (
+				trigger === "signIn" &&
+				account?.provider.toLowerCase() === "github"
+			) {
+				const res = await sendRequest<IBackendRes<JWT>>({
+					url: "http://localhost:8000/api/v1/auth/social-media",
+					method: "POST",
+					body: { category: "GITHUB", username: user.email },
+				});
+
+				if (res.data) {
+					token.access_token = res.data.access_token;
+					token.refresh_token = res.data.refresh_token;
+					token.user = res.data.user;
+				}
+			}
+			return token;
+		},
+		session({ session, token, user }) {
+			if (token) {
+				session.access_token = token.access_token;
+				session.refresh_token = token.refresh_token;
+				session.user = token.user;
+			}
+			return session;
+		},
+	},
 };
 
 const handler = NextAuth(authOptions);
