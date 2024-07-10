b8cfd14d82b91cb625bb3adeeefe5027345bce2a
Edvo1901 <DongVo1901@gmail.com>
2024-07-11 02:38:03 +0930

    add page title

--- src/app/(user)/track/upload/page.tsx
+++ src/app/(user)/track/upload/page.tsx
@@ -1,6 +1,12 @@
 "use client"
 import UploadTab from "@/components/track/UploadTab";
 import { Container } from "@mui/material";
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Upload new track',
+    description: 'new track upload page',
+}
 
 const UploadPage = () => {
 	return (
